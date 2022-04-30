92ffef658355cf84c5cbc39e83e21fea150ccf13
sawdahoque234 <sawdahoque6066@gmail.com>
2022-04-30 23:43:48 +0600

    accordiaon

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Do I have to allow the use of cookie?',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptatum earum doloribus! Expedita recusandae iure eligendi, quasi saepe ullam at.'
+    },
+    {
+      id: 2,
+      title: 'How do I change my My Page password?',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptatum earum doloribus! Expedita recusandae iure eligendi, quasi saepe ullam at.'
+    },
+    {
+      id: 3,
+      title: 'What is BankID?',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptatum earum doloribus! Expedita recusandae iure eligendi, quasi saepe ullam at.'
+       
+    },
+    {
+      id: 4,
+      title: 'Whose birth number can I use?',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptatum earum doloribus! Expedita recusandae iure eligendi, quasi saepe ullam at.'
+    },
+    {
+      id: 5,
+      title: 'When do I recieve a password ordered by letter?',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptatum earum doloribus! Expedita recusandae iure eligendi, quasi saepe ullam at.'
+    },
+  ]
+  export default questions
